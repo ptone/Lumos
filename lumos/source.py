c8c610c7249100e3e514b029a2f4209866910f3a
Preston Holmes <preston@ptone.com>
2013-10-29 20:44:45 -0700

    Allow a specific address to be specified for sending

--- lumos/source.py
+++ lumos/source.py
@@ -9,19 +9,27 @@ import struct
 
 from packet import E131Packet
 
+
 def ip_from_universe(universe):
     # derive multicast IP address from Universe
     high_byte = (universe >> 8) & 0xff
     low_byte = universe & 0xff
     return "239.255.{}.{}".format(high_byte, low_byte)
 
-class DMXSource(object):
 
-    def __init__(self, universe=1, network_segment=1):
+class DMXSource(object):
+    """
+    bind_ip is the IP address assigned to a specific HW interface
+    """
+
+    def __init__(self, universe=1, network_segment=1, bind_ip=None):
         self.universe = universe
         self.ip = ip_from_universe(universe)
         # open UDP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        if bind_ip:
+            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF,
+                    socket.inet_aton(bind_ip))
         # set ttl to limit network segment reach
         ttl = struct.pack('b', network_segment)
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
